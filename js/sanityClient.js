1e858929e4447c0ec6ea7a39f2348b098dcdc1f9
Mashcrypt <109661499+Mashcrypt@users.noreply.github.com>
2026-01-08 18:48:05 +0200

    Update sanityClient.js

--- js/sanityClient.js
+++ js/sanityClient.js
@@ -1,10 +1,9 @@
-// js/sanityClient.js
 import { createClient } from 'https://cdn.skypack.dev/@sanity/client';
 
 export const sanityClient = createClient({
-  projectId: 'qjg5raj1',    // YOUR Sanity Project ID
-  dataset: 'production',     // your dataset
-  apiVersion: '2026-01-08',  // use current date or version
-  useCdn: true               // `true` = fast, cached data; `false` = fresh data
+  projectId: 'qjg5raj1',
+  dataset: 'production',
+  apiVersion: '2024-01-01',
+  useCdn: true
 });
 
