a354c94427b6a69bfd2dc5c2017e37711af59cd9
Mashcrypt <109661499+Mashcrypt@users.noreply.github.com>
2026-01-08 20:38:33 +0200

    Update sanityClient.js

--- js/sanityClient.js
+++ js/sanityClient.js
@@ -1,12 +1,7 @@
-// sanityClient.js
-import { createClient } from 'https://cdn.sanity.io/client/bundle-2.38.3/sanityClient.min.js';
-
-// Career Unified Sanity client
-export const sanityClient = createClient({
-  projectId: 'qjg5raj1',       // Your Sanity project ID
-  dataset: 'production',        // Your dataset
-  useCdn: true,                 // `true` for public, cached data; `false` for fresh data
-  apiVersion: '2024-01-01',     // Sanity API version
-  token: '',                    // Leave empty for public read-only access
+export const sanityClient = sanityClient({
+    projectId: 'qjg5raj1',
+    dataset: 'production',
+    apiVersion: '2026-01-08', // today’s date or current
+    useCdn: true, // for faster read-only queries
 });
 
