b955bbc73c09e10df8439343db751c952c1a00e3
Mashcrypt <109661499+Mashcrypt@users.noreply.github.com>
2026-02-10 17:57:47 +0200

    Update sanity.ts

--- netlify/lib/sanity.ts
+++ netlify/lib/sanity.ts
@@ -3,24 +3,26 @@ export async function getJob(jobId: string) {
   const DATASET = "production";
 
   const query = `
-  *[_type == "job" && _id == "${jobId}"][0]{
-    _id,
-    title,
-    description,
-    location,
-    salary,
-    posted,
-    deadline,
-    "companyName": company,
-    "companyLogo": companyLogo.asset->url
-  }
-  `;
+*[_type == "job" && _id == "${jobId}"][0]{
+  _id,
+  title,
+  description,
+  location,
+  salary,
+  applyLink,
+  posted,
+  deadline,
+
+  "companyName": company->name,
+  "companyLogo": company->logo.asset->url
+}
+`;
 
   const url = `https://${PROJECT_ID}.api.sanity.io/v2023-08-01/data/query/${DATASET}?query=${encodeURIComponent(query)}`;
-
   const res = await fetch(url);
   const json = await res.json();
 
   return json.result;
 }
+
 
