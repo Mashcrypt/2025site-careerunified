a2781df74477c4073f5d251a2248e03a87782662
Mashcrypt <109661499+Mashcrypt@users.noreply.github.com>
2026-02-10 17:09:00 +0200

    Create company.js

---
+++ sanity/schemaTypes/company.js
@@ -0,0 +1,28 @@
+export default {
+  name: 'company',
+  title: 'Company',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      title: 'Company Name',
+      type: 'string',
+      validation: Rule => Rule.required()
+    },
+    {
+      name: 'logo',
+      title: 'Company Logo',
+      type: 'image',
+      options: {
+        hotspot: true
+      },
+      validation: Rule => Rule.required()
+    }
+  ],
+  preview: {
+    select: {
+      title: 'name',
+      media: 'logo'
+    }
+  }
+};
