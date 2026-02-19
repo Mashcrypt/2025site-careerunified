e2ae0c00fe2d016f62751591f0bbb96be8423812
Mashcrypt <109661499+Mashcrypt@users.noreply.github.com>
2026-02-19 12:26:56 +0200

    Update sanity.config.js

--- sanity.config.js
+++ sanity.config.js
@@ -1,7 +1,7 @@
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemaTypes'
+import {schemaTypes} from './sanity/schemaTypes'
 
 export default defineConfig({
   name: 'default',
@@ -16,3 +16,4 @@ export default defineConfig({
     types: schemaTypes,
   },
 })
+
